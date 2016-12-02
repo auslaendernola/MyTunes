90f05f0a6b62806884e1e309538dfb9836bf25e6
Tyler Black Hernandez <tylerblackhernandez@gmail.com>
2016-12-01 18:19:07 -0600

    All tests passing

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,28 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
 
+  initialize: function(response) {
+    this.fetch({reset: true});
+  },
+
+  url: 'https://api.parse.com/1/classes/songs/',
+
+  parse: function(response) {
+    console.log(response, 'response');
+    return response.results;
+  }
 });
+
+// var song = new Songs();
+//
+// song.fetch({
+//   success: function(response, xhr) {
+//     console.log(response + ' inside success');
+//   },
+//
+//   error: function(errorResponse) {
+//     console.log('inside error');
+//   }
+// });
